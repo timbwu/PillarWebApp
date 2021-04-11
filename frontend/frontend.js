5cff724671e29337d61e734d4ad0972e2a3960f0
le-rich <richardle1997@gmail.com>
2021-04-10 18:45:20 -0700

    cors

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -7,6 +7,13 @@ const endPointRoot = "/api/v1"
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+    next();
+})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/frontend/', express.static(path.join(__dirname, './')))
